4ddc31c179b035c7daf8aa12a02d759a5e9db0dc
Phúc Kseven Duy <you@example.com>
2022-10-20 11:03:52 +0700

    upload source code

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '10A1';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Các Bạn Nữ 10A1';
+const contentCard = 'Chúc các bạn 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'image.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
